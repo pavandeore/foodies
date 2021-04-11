4720a5cee5636a7bf603b860459d02de55f6f0c6
pawan deore <pavandeore90@gmail.com>
2021-04-11 08:38:47 +0530

    build

---
+++ build/precache-manifest.32ce42b6f4650f2dfef946d75fb73bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4efc9ba099d98a385dbf36d5eab1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "991f21b8625cc679569a",
+    "url": "/static/css/main.2f3f30e9.chunk.css"
+  },
+  {
+    "revision": "891dec42153f23a68bce",
+    "url": "/static/js/2.d89fda97.chunk.js"
+  },
+  {
+    "revision": "1edf6fec8b31b48da7686fe422ea0457",
+    "url": "/static/js/2.d89fda97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991f21b8625cc679569a",
+    "url": "/static/js/main.8711b038.chunk.js"
+  },
+  {
+    "revision": "c8f8e59f0e686c8bc130",
+    "url": "/static/js/runtime-main.db242772.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/NotFound.6695af99.svg"
+  }
+]);
